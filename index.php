e5784543f21575f82fa355b439642c99a48675fa
Ihsan Muhamad Sobari <isan.ms32@gmail.com>
2022-02-12 12:40:43 +0700

    project1

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Praktikum 1</title>
+    <link rel="stylesheet" href="style.css">
+    
+</head>
+<body>
+
+    <center><p style="font-size: 15px; color: #a09393; line-height: 1.6;">Category</p>
+    <center><h1 style="font-family: Arial, Helvetica, sans-serif;">Duh, MU Cuma Bikin 2 Gol dari Sepak </h1></center>
+    <center><h1 style="line-height: 1; font-family: Arial, Helvetica, sans-serif;">Pojok Selama Setahun Terakhir</h1></center>
+    <h1 class="by" style="font-size: 10pt; margin-top: -10pt;">By: [Ihsan Muhamad Sobari] | on 08 February 2022 | [2010631170080]</h1>
+    
+   <h3 class="Daftar" style="text-align: justify; text-indent: 2.8in;">Daftar Isi</h3>
+   <p class="satu" style="text-align: justify; text-indent: 2.9in;">1. <a href="https://sport.detik.com/sepakbola/liga-inggris/d-5933336/duh-mu-cuma-bikin-2-gol-dari-sepak-pojok-selama-setahun-terakhir?_ga=2.164408767.1262090608.1644289051-1929524763.1644201332">Introduction</a></p>
+   <p class="dua" style="text-align: justify; text-indent: 2.9in;">2. <a href="https://sport.detik.com/sepakbola/liga-inggris/d-5933336/duh-mu-cuma-bikin-2-gol-dari-sepak-pojok-selama-setahun-terakhir?_ga=2.164408767.1262090608.1644289051-1929524763.1644201332">Detail History</a></p>
+   <p class="tiga" style="text-align: justify; text-indent: 2.9in;">3. <a href="https://sport.detik.com/sepakbola/liga-inggris/d-5933336/duh-mu-cuma-bikin-2-gol-dari-sepak-pojok-selama-setahun-terakhir?_ga=2.164408767.1262090608.1644289051-1929524763.1644201332">Result</a></p>
+
+   <h2 class="Introduction" style="text-align: justify; text-indent: 2.8in; font-family: Arial, Helvetica, sans-serif;">Introduction</h2>
+        <p class="int" style="text-align: justify; text-indent: 2.9in;">Jakarta - Manchester United ternyata tim yang paling buruk dalam memanfaatkan sepak pojok di Liga Inggris setahun terakhir. Setan</p>
+        <p class="o" style="text-align: justify; text-indent: 2.9in;">berada di urutan paling buncit! Sky Sports merilis data mengenai tim-tim yang  paling produktif mencetak gol dalam Merah situasi</p>
+        <p class="p" style="text-align: justify; text-indent: 2.9in;">sepak pojok selama setahun terakhir, terhitung dari 28 Januari 2021 hingga 28 Januari 2022.Kriteria situasi sepak pojok dihitung saat </p>
+        <p class="q" style="text-align: justify; text-indent: 2.9in;"> barisan belakang tim bertahan masih terbentuk setelah bola ditendang dari tiang jauh dan tim menyerang masih berkumpul di kotak penalti lawan </p>
+        <p class="r" style="text-align: justify; text-indent: 2.9in;"></p>
+        
+    <h2 class="Detail" style="text-align: justify; text-indent: 2.8in; font-family: Arial, Helvetica, sans-serif;">Detail History</h2>
+        <p class="dtl" style="text-align: center; text-indent: 2in;">Hasilnya, MU hanya mencetak 2 gol saja dalam setahun terakhir lewat situasi sepak pojok. Satu di antaranya terjadi lewat gol bunuh diri</p>
+        <p class="s" style="text-align: justify; text-indent: 2.9in;">Craig Dawson saat melawan West Ham United pada Maret lalu. Satu gol lagi terjadi di bulan April melawan Burnley.Artinya di musim ini MU </p>
+        <p class="t" style="text-align: justify; text-indent: 2.9in;">belum mencetak gol lewat corner kick di Liga Inggris. Padahal, mereka terbilang punya banyak kesempatan. Selama setahun ini, Harry Maguire </p>
+        <p class="u" style="text-align: justify; text-indent: 2.9in;">dkk mendapat 210 sepak pojok, tertinggi ketujuh di Liga Inggris, namun hanya 66 tembakan yang bisa dilakukan. Artinya, rasio gol MU dari sepak </p>
+        <p class="v" style="text-align: justify; text-indent: 2.9in;">pojok tak sampai satu persen. Ini menjadi yang terburuk di Liga Inggris.</p>
+
+    <h2 class="Detail" style="text-align: justify; text-indent: 2.8in; font-family: Arial, Helvetica, sans-serif;">Result</h2>
+        <p class="w" style="text-align: justify; text-indent: 2.9in;">Hasilnya, MU hanya mencetak 2 gol saja dalam setahun terakhir lewat situasi sepak pojok. Satu di antaranya terjadi lewat gol bunuh diri</p>
+        <p class="x" style="text-align: justify; text-indent: 2.9in;">Craig Dawson corner saat melawan West Ham United pada Maret lalu. Satu gol lagi terjadi di bulan April melawan Burnley. Artinya di musim</p>
+        <p class="y" style="text-align: justify; text-indent: 2.9in;">ini MU belum mencetak gol lewat di Liga kick di Liga Inggris. Padahal, mereka terbilang punya banyak kesempatan.Selama setahun ini, Harry </p>
+        <p class="y" style="text-align: justify; text-indent: 2.9in;">Maguire dkk mendapat 210 sepak pojok, tertinggi ketujuh Inggris, namun hanya 66 tembakan yang bisa dilakukan.Artinya, rasio gol MU dari sepak</p>
+        <p class="y" style="text-align: justify; text-indent: 2.9in;">pojok tak sampai satu persen. Ini menjadi yang  terburuk di Liga Inggris.</p>
+</body>
+</html>
